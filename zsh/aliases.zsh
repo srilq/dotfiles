f24b19d0a47cc39168f70a38b9e60309957154db
Stef Jones <stef.jones@condenastint.com>
2018-01-28 12:51:17 +0000

    .

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,4 +11,6 @@ alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
 
+alias cls='clear'
+
 alias vlc='~/Applications/VLC.app/Contents/MacOS/VLC'
